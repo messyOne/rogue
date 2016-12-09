96bb19a3f5fab557a31e1f00a7cb92b8cc15b292
Daniel Weiss <daniel.weiss@innogames.com>
2016-12-09 15:52:58 +0100

    initial

---
+++ Assets/Script/Loader.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.Serialization.Formatters;
+
+public class Loader : MonoBehaviour
+{
+
+    public GameObject gameManager;
+
+    void Awake()
+    {
+        if (GameManager.instance == null)
+        {
+            Instantiate(gameManager);
+        }
+    }
+}
